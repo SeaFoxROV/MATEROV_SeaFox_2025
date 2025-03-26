7a85379d3e1c4d18c11166b9d6f5b434cb947cdd
SeaFoxInventive <seafoxROV@gmail.com>
2025-03-26 14:32:21 -0700

    4camera gui

--- src/ros2_seafox_package/ros2_seafox_package/base/gui/camera_gui.py
+++ src/ros2_seafox_package/ros2_seafox_package/base/gui/camera_gui.py
@@ -50,22 +50,22 @@ class CameraGUI(QWidget):
 
         # Window setup
         self.setWindowTitle('ROV Camera Viewer')
-        self.setFixedSize(1400, 600)
+        self.setFixedSize(1320, 960)
 
         # Dropdown selector
         self.dropdown = QComboBox()
         self.dropdown.addItems([
-            'Camera 1',
-            'Camera 2',
-            'Camera 3',
-            'Camera 4'
+            'Camera 1 and 2',
+            'Camera 2 and 3',
+            'Camera 3 and 4',
+            'Camera 4 and 1'
         ])
         self.dropdown2 = QComboBox()
         self.dropdown2.addItems([
-            'Camera 1',
-            'Camera 2',
-            'Camera 3',
-            'Camera 4'
+            'Camera 1 and 2',
+            'Camera 2 and 3',
+            'Camera 3 and 4',
+            'Camera 4 and 1'
         ])
 
         self.dropdown.currentIndexChanged.connect(self.change_camera)
@@ -76,20 +76,36 @@ class CameraGUI(QWidget):
         self.label.setFixedSize(640, 480)  # Adjust as needed for your camera resolution
         self.label2 = QLabel()
         self.label2.setFixedSize(640, 480)  # Adjust as needed for your camera resolution
+        self.label3 = QLabel()
+        self.label3.setFixedSize(640, 480)  # Adjust as needed for your camera resolution
+        self.label4 = QLabel()
+        self.label4.setFixedSize(640, 480)  # Adjust as needed for your camera resolution
 
         # Layout
         layout = QVBoxLayout()
-        layout.addWidget(self.dropdown)
-        layout.addWidget(self.dropdown2)
+        dropsplitter = QSplitter(Qt.Horizontal)
+        dropsplitter.addWidget(self.dropdown)
+        dropsplitter.addWidget(self.dropdown2)
+        layout.addWidget(dropsplitter)
+
         splitter = QSplitter(Qt.Horizontal)
         splitter.addWidget(self.label)
         splitter.addWidget(self.label2)
         layout.addWidget(splitter)
+
+        #remove for only 2 cameras
+        splitter2 = QSplitter(Qt.Horizontal)
+        splitter2.addWidget(self.label3)
+        splitter2.addWidget(self.label4)
+        layout.addWidget(splitter2)
+
         self.setLayout(layout)
 
         # Camera index to display
         self.current_camera_index = 0
         self.current_camera_index2 = 1
+        self.current_camera_index3 = 2
+        self.current_camera_index4 = 3
 
         # Timer to update GUI
         self.timer = QTimer()
@@ -98,11 +114,15 @@ class CameraGUI(QWidget):
 
     def change_camera(self, index):
         self.current_camera_index = index
+        self.current_camera_index3 = (index+1) if self.current_camera_index < 3 else 0
 
-    def change_camera2(self, index):
-        self.current_camera_index2 = index
+    def change_camera2(self, index2):
+        self.current_camera_index2 = index2
+        self.current_camera_index4 = (index2+1) if self.current_camera_index2 < 3 else 0
+
 
     def update_image(self):
+        #top left
         frame = self.node.image_data[self.current_camera_index]
         if frame is not None:
             height, width, channel = frame.shape
@@ -112,9 +132,10 @@ class CameraGUI(QWidget):
             self.label.setPixmap(pixmap)
         else:
             # Optional: Show "No signal" image or text when no frame available
-            self.label.setText("No signal from camera.")
+            self.label.setText(f"\t\tNo signal from camera{self.current_camera_index+1}")
 
         frame2 = self.node.image_data[self.current_camera_index2]
+        #top right
         if frame2 is not None:
             height, width, channel = frame2.shape
             bytes_per_line = 3 * width
@@ -123,7 +144,31 @@ class CameraGUI(QWidget):
             self.label2.setPixmap(pixmap2)
         else:
             # Optional: Show "No signal" image or text when no frame available
-            self.label2.setText("No signal from camera.")
+            self.label2.setText(f"\t\tNo signal from camera{self.current_camera_index2+1}")
+        
+        frame3 = self.node.image_data[self.current_camera_index3]
+        #bottom left
+        if frame3 is not None:
+            height, width, channel = frame3.shape
+            bytes_per_line = 3 * width
+            q_img3 = QImage(frame3.data, width, height, bytes_per_line, QImage.Format_RGB888).rgbSwapped()
+            pixmap3 = QPixmap.fromImage(q_img3).scaled(self.label2.width(), self.label2.height())
+            self.label3.setPixmap(pixmap3)
+        else:
+            # Optional: Show "No signal" image or text when no frame available
+            self.label3.setText(f"\t\tNo signal from camera{self.current_camera_index3+1}")
+
+        frame4 = self.node.image_data[self.current_camera_index4]
+        #bottom right
+        if frame4 is not None:
+            height, width, channel = frame4.shape
+            bytes_per_line = 3 * width
+            q_img4 = QImage(frame4.data, width, height, bytes_per_line, QImage.Format_RGB888).rgbSwapped()
+            pixmap4 = QPixmap.fromImage(q_img4).scaled(self.label2.width(), self.label2.height())
+            self.label4.setPixmap(pixmap4)
+        else:
+            # Optional: Show "No signal" image or text when no frame available
+            self.label4.setText(f"\t\tNo signal from camera{self.current_camera_index4+1}")
 
 
 
