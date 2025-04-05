32111b4aaa4edd261153b7933481740f2bfd5a8b
Cedric <cedric.martinez@cetys.edu.mx>
2025-04-05 10:31:22 -0700

    Some changes to yolo

--- src/ros2_seafox_package/ros2_seafox_package/rov/cameras/yolo_model/yolo.py
+++ src/ros2_seafox_package/ros2_seafox_package/rov/cameras/yolo_model/yolo.py
@@ -5,8 +5,9 @@ from sensor_msgs.msg import Image
 from std_msgs.msg import Int32MultiArray
 from cv_bridge import CvBridge
 import cv2
+from ultralytics import YOLO
 
-class YOLO(Node):
+class Yolo(Node):
     def __init__(self):
         super().__init__('yolo')
         self.bridge = CvBridge()
@@ -21,14 +22,24 @@ class YOLO(Node):
         self.model = YOLO(r'src/ros2_seafox_package/ros2_seafox_package/rov/cameras/YOLO_model/best.pt')
 
     def image_callback(self, msg):
-        ret, frame = self.cap.read()
-        if not ret:
-            self.get_logger().error("Failed to capture image")
+        try:
+            # ROS2 to OpenCV
+            frame = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+        except Exception as e:
+            self.get_logger().error(f"Error converting image: {e}")
             return
         
         results = self.model(frame)
         annotated_frame = results[0].plot()
 
         cv2.imshow('YOLO Detection', annotated_frame)
+        cv2.waitKey(1)
+
+        try:
+            # OpenCV to ROS2
+            annotated_frame = self.bridge.cv2_to_imgmsg(annotated_frame, encoding='bgr8')
+        except Exception as e:
+            self.get_logger().error(f"Error converting image back to ROS2: {e}")
+            return
         # Publish the processed image
         self.image_publisher.publish(annotated_frame)
