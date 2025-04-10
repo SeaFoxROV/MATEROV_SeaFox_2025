fb8140faa22dc247ee8ee7014aa25aac446682ac
SeaFoxInventive <seafoxROV@gmail.com>
2025-04-10 14:05:05 -0700

    fixed .ino

--- src/ros2_seafox_package/launch/base_launcher.py
+++ src/ros2_seafox_package/launch/base_launcher.py
@@ -27,4 +27,31 @@ def generate_launch_description():
             name='newtons_to_pwm',
             output='screen',
         ),
+        #Camera nodes
+        Node(
+            package='ros2_seafox_package',  # Reemplaza con el nombre de tu paquete
+            executable='realsense',  # Reemplaza con el nombre del ejecutable de tu nodo
+            name='realsense',
+            output='screen',
+        ),
+        Node(
+            package='ros2_seafox_package',  # Reemplaza con el nombre de tu paquete
+            executable='camera_gui',  # Reemplaza con el nombre del ejecutable de tu nodo
+            name='camera_gui',
+            output='screen',
+        ),
+        Node(
+            package='ros2_seafox_package',  # Reemplaza con el nombre de tu paquete
+            executable='main_gui',  # Reemplaza con el nombre del ejecutable de tu nodo
+            name='main_gui',
+            output='screen',
+        ),
+        Node(
+            package='ros2_seafox_package',  # Reemplaza con el nombre de tu paquete
+            executable='imageViewer',  # Reemplaza con el nombre del ejecutable de tu nodo
+            name='imageViewer',
+            output='screen',
+        ),
+
+
     ])
