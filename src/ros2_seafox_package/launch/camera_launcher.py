33279b6b4c40f0a27af4dbb3c5b257be25380079
Zaracla2112 <ever.alcaraz@cetys.edu.mx>
2025-04-28 13:00:26 -0700

    merge

--- src/ros2_seafox_package/launch/camera_launcher.py
+++ src/ros2_seafox_package/launch/camera_launcher.py
@@ -11,6 +11,13 @@ def generate_launch_description():
         ),
         Node(
             package='ros2_seafox_package',  # Reemplaza con el nombre de tu paquete
+            executable='joystick_reader',  # Reemplaza con el nombre del ejecutable de tu nodo
+            name='joystick_reader',
+            output='screen',
+        ),
+
+        Node(
+            package='ros2_seafox_package',  # Reemplaza con el nombre de tu paquete
             executable='realsense',  # Reemplaza con el nombre del ejecutable de tu nodo
             name='realsense',
             output='screen',
