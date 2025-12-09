bed0520d17a285bb93fe8a0d29c4f503b56a7f4e
Zaracla2112 <ever.alcaraz@cetys.edu.mx>
2025-12-09 14:36:28 -0800

    fix the rov launcher

--- src/ros2_seafox_package/launch/rov_launcher.py
+++ src/ros2_seafox_package/launch/rov_launcher.py
@@ -1,9 +1,23 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
 
 def generate_launch_description():
+
+    # Declarar argumento camera_index (default = 0)
+    camera_index_arg = DeclareLaunchArgument(
+        'camera_index',
+        default_value='0',
+        description='Índice de la cámara (video index)'
+    )
+
+    # Crear objeto LaunchConfiguration
+    camera_index = LaunchConfiguration('camera_index')
+
     return LaunchDescription([
-        # Nodo de rosserial
+        camera_index_arg,
+
         Node(
             package='ros2_seafox_package',
             executable='rosserial',
@@ -11,12 +25,11 @@ def generate_launch_description():
             output='screen',
         ),
 
-        # Nodo de cámara con argumento para elegir índice
         Node(
             package='ros2_seafox_package',
             executable='camera_publisher',
             name='camera_publisher',
             output='screen',
-            arguments=['4'],   # <<< AQUÍ eliges la cámara (/dev/video4)
+            arguments=[camera_index],   # <-- AQUÍ ya funciona
         ),
     ])
